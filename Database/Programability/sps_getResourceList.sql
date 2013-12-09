04c13ca6b159691792a1795c6e446a0261d45042
pbekker <mxia69@gmail.com>
2013-12-09 21:35:24 +0200

    added sp for getting a generic list
    Might need to upgrade it later to include topic filtering....

---
+++ Database/Programability/sps_getResourceList.sql
@@ -0,0 +1,34 @@
+USE [justfyty_justfo]
+GO
+
+IF EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'sps_getResourceList'))
+BEGIN
+	DROP PROCEDURE dbo.sps_getResourceList
+END
+GO
+
+CREATE PROCEDURE dbo.sps_getResourceList
+	@active bit = 0
+AS
+BEGIN
+
+	SELECT 
+		r.id,
+		r.name,
+		l.name [language],
+		t.name [topic],
+		r.[description],
+		r.uploadDate,
+		r.uploadUser
+	
+	FROM bhdResource r	(NOLOCK)
+		JOIN bhdResourceLanguage l (NOLOCK) ON l.id = r.languageId
+		JOIN bhdResourceTopics t (NOLOCK) ON t.id = r.topicId
+	WHERE r.isActive = @active
+END
+GO
+
+
+GO
+
+EXEC sps_getResourceList
